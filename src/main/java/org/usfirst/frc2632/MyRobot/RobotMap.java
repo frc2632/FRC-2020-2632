53fd9671e9ce5238a7a702499584ba65da67142f
Sam McQuate <sammcq8@gmail.com>
2019-01-21 17:33:08 -0500

    Init Upload
    Adding I did before setting up the github into github

---
+++ src/main/java/org/usfirst/frc2632/MyRobot/RobotMap.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc2632.MyRobot;
+
+
+public class RobotMap{
+
+    public static final int LFMOTORCONTROLLER = 1;
+    public static final int LRMOTORCONTROLLER = 3;
+    public static final int RFMOTORCONTROLLER = 0;
+    public static final int RRMOTORCONTROLLER = 2;
+    public static final int HATCH_LIMITSWITCH1 = 5;
+
+    public static final int HATCH_SERVO1 = 0;
+}
